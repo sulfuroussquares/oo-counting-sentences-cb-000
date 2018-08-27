2581af083a0bcaf68629f485d90de77f50975708
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-08-27 05:47:42 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,8 +27,8 @@ class String
   end
 
   def count_sentences
-array1 = self.split
+array1 = self.split(/[.!?]/)
 number1 = array1.reject {|n| n.empty?}.count
-
+self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
 end
